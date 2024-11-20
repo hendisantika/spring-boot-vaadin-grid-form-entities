75919e4d65a00068d40841b81aefd621c30be234
hendisantika <hendisantika@yahoo.co.id>
2024-11-20 09:08:31 +0700

    Add BaseEntity🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vaadingridformentities/entity/base/BaseEntity.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.vaadingridformentities.entity.base;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-vaadin-grid-form-entities
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/11/24
+ * Time: 09.08
+ * To change this template use File | Settings | File Templates.
+ */
+public interface BaseEntity {
+}
