1ded50a2ae49705662edb93b8246c020d929eb50
hendisantika <hendisantika@yahoo.co.id>
2024-11-20 09:08:49 +0700

    Add FormField🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vaadingridformentities/entity/base/FormField.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.vaadingridformentities.entity.base;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-vaadin-grid-form-entities
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/11/24
+ * Time: 09.08
+ * To change this template use File | Settings | File Templates.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface FormField {
+    String label() default "";
+
+    int order() default 999;
+
+    boolean required() default false;
+}
