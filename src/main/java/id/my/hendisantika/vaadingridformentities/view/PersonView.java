cf2b8caff86582d7b276a4ea5635134a3a9821a9
hendisantika <hendisantika@yahoo.co.id>
2024-11-20 09:16:53 +0700

    Add PersonView🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vaadingridformentities/view/PersonView.java
@@ -0,0 +1,32 @@
+package id.my.hendisantika.vaadingridformentities.view;
+
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import id.my.hendisantika.vaadingridformentities.entity.Person;
+import id.my.hendisantika.vaadingridformentities.ui.GenericView;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-vaadin-grid-form-entities
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/11/24
+ * Time: 09.16
+ * To change this template use File | Settings | File Templates.
+ */
+@PageTitle("PERSONS")
+@Menu(order = 0, icon = "line-awesome/svg/home-solid.svg")
+@Route(value = "", layout = MainLayout.class)
+public class PersonView extends GenericView<Person> {
+
+    private final DataService dataService;
+
+
+    public PersonView(DataService dataService) {
+        super(Person.class);
+        this.dataService = dataService;
+        refreshGrid();
+    }
+}
