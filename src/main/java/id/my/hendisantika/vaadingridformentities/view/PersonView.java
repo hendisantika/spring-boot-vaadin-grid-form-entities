630561d596d07b88b6f508df36d04c059e0308fd
hendisantika <hendisantika@yahoo.co.id>
2024-11-21 06:38:21 +0700

    Add Button in PersonView 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vaadingridformentities/view/PersonView.java
+++ src/main/java/id/my/hendisantika/vaadingridformentities/view/PersonView.java
@@ -1,5 +1,7 @@
 package id.my.hendisantika.vaadingridformentities.view;
 
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -31,6 +33,15 @@ public class PersonView extends GenericView<Person> {
         super(Person.class);
         this.dataService = dataService;
         refreshGrid();
+
+        // Add Button for adding new person
+        Div addButton = new Div();
+        addButton.setClassName("circle-button-container");
+        Avatar addAvatar = new Avatar("+");
+        addAvatar.addClassName("circle-button");
+        addButton.add(addAvatar);
+        addButton.addClickListener(event -> addNew());
+        gridContainer.addComponentAsFirst(addButton);
     }
 
     @Override
