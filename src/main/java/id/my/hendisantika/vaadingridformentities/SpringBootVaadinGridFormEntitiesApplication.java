bc62c04bb55e25835d68fe0815e0d1a2a1e8b98e
hendisantika <hendisantika@yahoo.co.id>
2024-11-20 19:12:11 +0700

    Bug Fix🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vaadingridformentities/SpringBootVaadinGridFormEntitiesApplication.java
+++ src/main/java/id/my/hendisantika/vaadingridformentities/SpringBootVaadinGridFormEntitiesApplication.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.vaadingridformentities;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Theme(value = "vaadin-entity", variant = Lumo.DARK)
-public class SpringBootVaadinGridFormEntitiesApplication {
+public class SpringBootVaadinGridFormEntitiesApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootVaadinGridFormEntitiesApplication.class, args);
