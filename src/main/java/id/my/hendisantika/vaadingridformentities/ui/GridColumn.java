286317ee4b86633eb894c54e2b3d36be37ef583f
hendisantika <hendisantika@yahoo.co.id>
2024-11-20 09:13:06 +0700

    Add GridColumn🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vaadingridformentities/ui/GridColumn.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.vaadingridformentities.ui;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-vaadin-grid-form-entities
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/11/24
+ * Time: 09.12
+ * To change this template use File | Settings | File Templates.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface GridColumn {
+    String header() default "";
+
+    int order() default 999;
+
+    boolean sortable() default true;
+
+    String dateTimeFormat() default "";
+}
