fbfc1edae27efabe950b84eda128bb665ffa2e8b
hendisantika <hendisantika@yahoo.co.id>
2024-11-20 09:15:31 +0700

    Add addHeaderContent in MainLayout🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vaadingridformentities/view/MainLayout.java
+++ src/main/java/id/my/hendisantika/vaadingridformentities/view/MainLayout.java
@@ -1,9 +1,11 @@
 package id.my.hendisantika.vaadingridformentities.view;
 
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.router.Layout;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -27,5 +29,15 @@ public class MainLayout extends AppLayout {
         setPrimarySection(Section.DRAWER);
         addDrawerContent();
         addHeaderContent();
+    }
+
+    private void addHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.setAriaLabel("Menu toggle");
+
+        viewTitle = new H1();
+        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+
+        addToNavbar(true, toggle, viewTitle);
     }
 }
