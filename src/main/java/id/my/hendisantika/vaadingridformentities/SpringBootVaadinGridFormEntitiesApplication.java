e3f103b1df4f2ded1449c9c138ec6731eea44f8f
hendisantika <hendisantika@yahoo.co.id>
2024-11-20 12:38:34 +0700

    Still error🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vaadingridformentities/SpringBootVaadinGridFormEntitiesApplication.java
+++ src/main/java/id/my/hendisantika/vaadingridformentities/SpringBootVaadinGridFormEntitiesApplication.java
@@ -1,10 +1,12 @@
 package id.my.hendisantika.vaadingridformentities;
 
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@Theme(value = "vaadin-entity", variant = Lumo.DARK)
+@Theme(value = "vaadin-entity", variant = Lumo.DARK)
 public class SpringBootVaadinGridFormEntitiesApplication {
 
     public static void main(String[] args) {
