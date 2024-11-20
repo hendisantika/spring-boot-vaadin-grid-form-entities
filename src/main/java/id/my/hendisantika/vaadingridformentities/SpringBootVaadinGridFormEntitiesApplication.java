e4ae84d9c7b11cf8877a8588bbada9ade12f8d76
hendisantika <hendisantika@yahoo.co.id>
2024-11-20 09:05:18 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vaadingridformentities/SpringBootVaadinGridFormEntitiesApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.vaadingridformentities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVaadinGridFormEntitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVaadinGridFormEntitiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/vaadingridformentities/SpringBootVaadinGridFormEntitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.vaadingridformentities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVaadinGridFormEntitiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
